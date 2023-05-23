552a38150f689418d3d90afe95e4e75e0f87e1c5
Armando <armando.avelar@outlook.com>
2023-05-23 12:43:41 -0600

    Fix: add todos

---
+++ useTodo/todoReducer.js
@@ -0,0 +1,21 @@
+
+
+export const todoReducer = (initialState, action) => {
+
+    switch(action.type){
+        case 'Add':
+            return [...initialState, action.payload];
+        case 'Delete':
+            return initialState.filter(todo => todo.id !== action.payload);
+        case 'Toggle':
+            return initialState.map((todo) => {
+                if(todo.id == action.payload){
+                    todo.done = !todo.done;
+                }
+                return todo;
+            })
+        default:
+            return initialState;
+    }
+
+}
