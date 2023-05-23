1951de418a9cbca7208cd48a1f3e2dd982220b71
Armando <armando.avelar@outlook.com>
2023-05-23 12:35:41 -0600

    Primer commit

---
+++ useTodo.js
@@ -0,0 +1,36 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+const init = () => {
+    return JSON.parse(localStorage.getItem('todos')) || [];
+}
+
+export const useTodo = () => {
+
+    const [todos, dispatch] = useReducer(todoReducer, init());
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos));
+    }, [todos]);
+
+    const handleNewTodo = (todo) => {
+        dispatch({ type: 'Add', payload: todo });
+    }
+
+    const handleDeleteTodo = (id) => {
+        dispatch({ type: 'Delete', payload: id });
+    }
+
+    const handleToogleTodo = (id) => {
+        dispatch({ type: 'Toggle', payload: id });
+    }
+
+    return {
+        todos,
+        handleNewTodo,
+        handleDeleteTodo,
+        handleToogleTodo,
+        todosCount: todos.length,
+        pendingTodosCount: todos.filter(todo => !todo.done).length
+    }
+}
